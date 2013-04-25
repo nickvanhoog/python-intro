db3f7a1bb3e5fdde4857b6c31dc8bfb1bcfe08c9
Nick Van Hoogenstyn <nickvanhoog@gmail.com>
2013-04-24 22:01:42 -0700

    Putting first_script.py

---
+++ first_script.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import sys # sys is a "module", this is analogous to #include <iostream>
+
+# A function is defined with the def keyword.
+def main():
+	# Python statements do not need a semicolon at the end!
+
+	x = 3 # Don't need to declare variables. And variables don't have strict types.
+	x = "Now x is a string" # You can assign any new value to an existing variable.
+	x = 'x is still a string' # Strings can be written with single or double quotes
+
+	printArg(x) # Function calls are just as you'd expect
+
+	num = 0;
+	while (num < 3):
+		#Indentation is not optional!
+		print "num is now: {}".format(num) # Use str.format() to put values into strings
+		num += 1 # No increment (num++) / decrement (num--) operator
+
+	# if statements work as expected; note the "elif" instead of "else if"
+	if num is None: # None is analogous to NULL in C++
+		print "num is None"
+	elif num is not None:
+		print "num is not None"
+	else:
+		print "I just wanted to show an else section"
+
+
+	# Exceptions are cool! Use them!
+	try:
+		print y # y has not been defined; notice this was only caught once we ran this code
+	except NameError: # NameError is a built-in Python exception
+		print 'Caught exception'
+
+	return 0 # A function without a return value implicitly returns None
+
+def printArg(arg):
+	print arg # To print something, just write "print <thing you want to print>" (without the < >)
+
+if __name__ == '__main__': # Calling this with "python first_script.py" sets __name__ to '__main__'
+	main()
